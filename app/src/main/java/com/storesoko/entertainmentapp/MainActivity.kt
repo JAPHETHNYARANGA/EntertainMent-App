db7a388a31c5016b7df7249e1653d7ca63a91c36
JAPHETHNYARANGA <nyaranga4@gmail.com>
2022-03-22 15:41:09 +0300

    Finished Creating DrawerActivity

--- app/src/main/java/com/storesoko/entertainmentapp/MainActivity.kt
+++ app/src/main/java/com/storesoko/entertainmentapp/MainActivity.kt
@@ -2,10 +2,45 @@ package com.storesoko.entertainmentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle:ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout,R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.get_news-> Toast.makeText(applicationContext, "clicked get name", Toast.LENGTH_SHORT).show()
+                R.id.get_movies -> Toast.makeText(applicationContext, "clicked get name", Toast.LENGTH_SHORT).show()
+                R.id.get_age -> Toast.makeText(applicationContext, "clicked get name", Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
