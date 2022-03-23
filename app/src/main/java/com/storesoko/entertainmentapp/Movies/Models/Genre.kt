184f6dea6085660cbb34f6898df1265c6c4343a4
JAPHETHNYARANGA <nyaranga4@gmail.com>
2022-03-23 15:28:14 +0300

    created Movies Models

---
+++ app/src/main/java/com/storesoko/entertainmentapp/Movies/Models/Genre.kt
@@ -0,0 +1,6 @@
+package com.storesoko.entertainmentapp.Movies.Models
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/storesoko/entertainmentapp/Movies/Models/ProductionCountry.kt
@@ -0,0 +1,6 @@
+package com.storesoko.entertainmentapp.Movies.Models
+
+data class ProductionCountry(
+    val iso_3166_1: String,
+    val name: String
+)
