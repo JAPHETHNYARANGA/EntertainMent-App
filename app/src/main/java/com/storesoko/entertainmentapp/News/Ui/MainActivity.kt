a080173b4382f6a31e2f15a27cad74f40481682e
JAPHETHNYARANGA <nyaranga4@gmail.com>
2022-04-05 14:13:59 +0300

    Added logout functionality in MainActivity

--- app/src/main/java/com/storesoko/entertainmentapp/News/Ui/MainActivity.kt
+++ app/src/main/java/com/storesoko/entertainmentapp/News/Ui/MainActivity.kt
@@ -10,14 +10,20 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.storesoko.entertainmentapp.Age.Ui.AgeActivity
+import com.storesoko.entertainmentapp.LoginActivity
 import com.storesoko.entertainmentapp.Movies.Ui.MoviesActivity
 import com.storesoko.entertainmentapp.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var toggle:ActionBarDrawerToggle
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,11 +38,14 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        auth = Firebase.auth
+
         navView.setNavigationItemSelectedListener {
             when(it.itemId){
                 R.id.get_news -> startActivity(Intent(applicationContext, MainActivity::class.java))
                 R.id.get_movies -> startActivity(Intent(applicationContext, MoviesActivity::class.java))
                 R.id.get_age -> startActivity(Intent(applicationContext, AgeActivity::class.java))
+                R.id.logout -> logout()
             }
 
             true
@@ -45,6 +54,12 @@ class MainActivity : AppCompatActivity() {
         //bottom navigation
         bottonNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
 
+    }
+
+    private fun logout() {
+        Firebase.auth.signOut()
+        startActivity(Intent(applicationContext, LoginActivity::class.java))
+        finish()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
